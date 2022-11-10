eee5677026a3f871abdb5fe3a8b25cae009a5644
Travis Nagle <itsnagle@gmail.com>
2022-11-10 09:30:04 -0500

    Driver adjustments

--- Project5Zork/GameDriver.cs
+++ Project5Zork/GameDriver.cs
@@ -7,8 +7,51 @@ namespace Project5Zork
         public static void Main()
         {
             Player player = new Player();
+            Monster monster = new Monster();
 
             Console.WriteLine(player);
+            Console.WriteLine();
+            Console.WriteLine(monster);
+
+            Menu();
+        }
+
+        public static void Menu()
+        {
+            int userChoice = -1;
+            bool validChoice = false;
+
+            while(!validChoice)
+            {
+                try
+                {
+                    Console.WriteLine("-------ZORK-------");
+                    Console.WriteLine();
+
+                    Console.WriteLine("1. Play Game\n2. Exit Game");
+                    userChoice = int.Parse(Console.ReadLine());
+                    validChoice = true;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine($"\nThat is not a valid option, please try again.\n");
+                }
+            }
+
+            switch(userChoice)
+            {
+                case 1:
+                    PlayGame();
+                    break;
+                case 2:
+                    Console.WriteLine("Thank you for playing!");
+                    break;
+            }
+        }
+
+        public static void PlayGame()
+        {
+
         }
     }
 }
