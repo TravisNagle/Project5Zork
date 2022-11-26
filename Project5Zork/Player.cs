6c3dda741c469fafb815e41cd56bef7929057da2
Travis Nagle <itsnagle@gmail.com>
2022-11-26 18:42:05 -0500

    Player documentation

--- Project5Zork/Player.cs
+++ Project5Zork/Player.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////
+//
+// Project: Project5Zork
+// File Name: Player.cs
+// Description: Player object that keeps track of player info (Health, Weapon, Name)
+// Course: CSCI 1260-001 – Introduction to Computer Science II
+// Author: Travis Nagle, naglet@etsu.edu, Department of Computing, East Tennessee State University
+// Created: 11/26/2022
+// Copyright: Travis Nagle, 2022
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +20,19 @@ using System.Threading.Tasks;
 
 namespace Project5Zork
 {
+    /// <summary>
+    /// Implementation of the Player class that inherits from Participant. 
+    /// Created once the player starts the game.
+    /// </summary>
     public class Player : Participant
     {
         public bool HasStick { get; set; } = false;
         public bool HasSword { get; set; } = false;
+
+        /// <summary>
+        /// Default Player constructor that creates the player with the standard
+        /// amount of health an "Unarmed" weapon.
+        /// </summary>
         public Player() : base()
         {
             Dead = false;
@@ -18,9 +41,17 @@ namespace Project5Zork
             Weapon = new Weapon(5, "Unarmed");
         }
 
+        /// <summary>
+        /// Overrides the CalcDamage abstract method from Participant that
+        /// calculates the number of damage the player has taken and, if 
+        /// health reaches <= 0, the player is considered "dead".
+        /// </summary>
+        /// <param name="player">The player</param>
+        /// <param name="monster">The monster the player is fighting</param>
+        /// <returns>The health of the player at the end of each attack</returns>
         public override int CalcDamage(Player player, Monster monster)
         {
-            Health = Health - 100;
+            Health = Health - 4;
             Console.WriteLine($"The Player has taken damage!\nHealth: {Health}");
 
             if (Health <= 0)
@@ -31,6 +62,10 @@ namespace Project5Zork
             return Health;
         }
 
+        /// <summary>
+        /// GetWeapon method that sets the players weapon to whichever
+        /// weapon the player has picked up.
+        /// </summary>
         public void GetWeapon()
         {
             if (HasStick)
@@ -39,6 +74,10 @@ namespace Project5Zork
                 Weapon = new Weapon(8, "Sword");
         }
 
+        /// <summary>
+        /// Player ToString method that displays the players information
+        /// </summary>
+        /// <returns>Player info as a string</returns>
         public override string ToString()
         {
             return base.ToString();
